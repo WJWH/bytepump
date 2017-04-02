c13227253b68fe8850d7b5a76cbf02716816a8e9
Wander Hillen <wjw.hillen@gmail.com>
2017-04-02 18:30:00 +0200

    Set up of minitest

--- test/bytepump_test.rb
+++ test/bytepump_test.rb
@@ -1,11 +1,21 @@
 require 'test_helper'
+require 'bytepump'
+require 'socket'
+require 'io/nonblock'
 
 class BytepumpTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Bytepump::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def setup
+  end
+  
+  def teardown
+  end
+  
+  def test_it_splices_between_files
+    
+    assert true
   end
 end
